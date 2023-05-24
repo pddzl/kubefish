b38576b343b4a3fc031fe19bd90ecbb6f740d41f
pddzl <pddzl5@163.com>
2023-05-24 19:59:06 +0800

    分页调整

--- web/src/hooks/usePagination.ts
+++ web/src/hooks/usePagination.ts
@@ -29,13 +29,13 @@ export function usePagination(initialPaginationData: PaginationData = {}) {
   /** 合并分页参数 */
   const paginationData = reactive({ ...defaultPaginationData, ...initialPaginationData })
   /** 改变当前页码 */
-  const handleCurrentChange = (value: number) => {
+  const changeCurrentPage = (value: number) => {
     paginationData.currentPage = value
   }
   /** 改变页面大小 */
-  const handleSizeChange = (value: number) => {
+  const changePageSize = (value: number) => {
     paginationData.pageSize = value
   }
 
-  return { paginationData, handleCurrentChange, handleSizeChange }
+  return { paginationData, changeCurrentPage, changePageSize }
 }
