a8319fc473207fcacd6b7ed26f6931ee0c306bc8
pddzl <kubeops@163.com>
2023-06-21 00:27:59 +0800

    SecretDetail功能

--- web/src/views/k8s/config/secret/detail.vue
+++ web/src/views/k8s/config/secret/detail.vue
@@ -1,44 +1,28 @@
 <template>
   <div class="app-container">
     <div class="detail-operation">
-      <el-button icon="view" type="primary" plain @click="viewOrchFunc">查看</el-button>
-      <el-button icon="delete" type="danger" plain @click="deleteFunc">删除 </el-button>
+      <div class="detail-operation">
+        <el-button icon="view" type="primary" plain @click="viewOrchFunc">查看</el-button>
+        <el-button icon="delete" type="danger" plain @click="deleteFunc">删除 </el-button>
+      </div>
     </div>
     <div class="kop-collapse">
       <el-collapse v-model="activeNames">
         <el-collapse-item v-if="detail.metadata" title="元数据" name="metadata">
-          <MetaData :metadata="detail.metadata" />
+          <meta-data :metadata="detail.metadata" />
         </el-collapse-item>
-        <el-collapse-item title="数据" name="data">
-          <div style="padding: 20px; border-radius: 4px; background-color: #f9fbfd; margin-right: 20px">
+        <el-collapse-item v-if="detail.data" title="数据" name="data">
+          <div class="data-container">
             <div v-for="(value, key) in detail.data" :key="key">
-              <span
-                style="
-                  font-size: 14px;
-                  font-weight: 600;
-                  font-style: normal;
-                  color: #242e42;
-                  text-shadow: 0 4px 8px rgba(36, 46, 66, 0.1);
-                "
-                >{{ key }}</span
-              >
-              <pre
-                style="
-                  margin-top: 8px;
-                  padding: 12px;
-                  border-radius: 4px;
-                  background-color: #fff;
-                  border: 1px solid #e3e9ef;
-                "
-                >{{ value }}</pre
-              >
+              <span class="key-container">{{ key }}</span>
+              <pre class="value-container">{{ dataDecode(value) }}</pre>
             </div>
           </div>
         </el-collapse-item>
       </el-collapse>
     </div>
     <el-dialog v-model="dialogFormVisible" title="查看资源" width="55%">
-      <vue-code-mirror v-model:modelValue="formatData" :readOnly="true" />
+      <VueCodeMirror v-model:modelValue="formatData" :readOnly="true" />
     </el-dialog>
   </div>
 </template>
@@ -46,14 +30,14 @@
 <script lang="ts" setup>
 import { ref } from "vue"
 import { useRoute } from "vue-router"
-import { getConfigMapDetailApi, deleteConfigMapApi } from "@/api/k8s/config/configMap"
+import { getSecretDetailApi, deleteSecretApi } from "@/api/k8s/config/secret"
 import VueCodeMirror from "@/components/codeMirror/index.vue"
 import MetaData from "@/components/k8s/metadata.vue"
 import { ElMessage, ElMessageBox } from "element-plus"
 import { viewOrch } from "@/utils/k8s/orch"
 
 defineOptions({
-  name: "ConfigMapList"
+  name: "SecretDetail"
 })
 
 // 折叠面板
@@ -64,11 +48,11 @@ const route = useRoute()
 const namespace = route.query.namespace as string
 const name = route.query.name as string
 
-// 获取configMap详情
+// secret detail
 const detail = ref({ metadata: {}, data: {} })
 
 const getDetailData = async () => {
-  await getConfigMapDetailApi({ namespace: namespace, name: name }).then((res) => {
+  await getSecretDetailApi({ namespace: namespace, name: name }).then((res) => {
     if (res.code === 0) {
       detail.value = res.data
     }
@@ -80,18 +64,18 @@ getDetailData()
 const dialogFormVisible = ref(false)
 const formatData = ref<string>("")
 const viewOrchFunc = async () => {
-  formatData.value = await viewOrch(name, "configmaps", namespace)
+  formatData.value = await viewOrch(name, "secrets", namespace)
   dialogFormVisible.value = true
 }
 
 // 删除
 const deleteFunc = async () => {
-  ElMessageBox.confirm("此操作将永久删除该ConfigMap, 是否继续?", "提示", {
+  ElMessageBox.confirm("此操作将永久删除该Secret, 是否继续?", "提示", {
     confirmButtonText: "确定",
     cancelButtonText: "取消",
     type: "warning"
   }).then(async () => {
-    const res = await deleteConfigMapApi({ namespace: namespace, name: name })
+    const res = await deleteSecretApi({ namespace: namespace, name: name })
     if (res.code === 0) {
       ElMessage({
         type: "success",
@@ -100,4 +84,36 @@ const deleteFunc = async () => {
     }
   })
 }
+
+// 解码
+const dataDecode = (str: string) => {
+  return atob(str)
+}
 </script>
+
+<style lang="scss" scoped>
+.data-container {
+  padding: 15px;
+  border-radius: 4px;
+  background-color: #f9fbfd;
+  margin-right: 20px;
+}
+
+.key-container {
+  font-size: 14px;
+  font-weight: 600;
+  font-style: normal;
+  color: #242e42;
+  text-shadow: 0 4px 8px rgba(36, 46, 66, 0.1);
+}
+
+.value-container {
+  margin-top: 8px;
+  padding: 12px;
+  border-radius: 4px;
+  background-color: #fff;
+  border: 1px solid #e3e9ef;
+  word-break: break-all;
+  white-space: pre-wrap;
+}
+</style>
